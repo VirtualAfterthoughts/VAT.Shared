ec0b29b6ef631686a7f136e94717564e800f2911
Logan Shockley <loganv182@gmail.com>
2025-07-05 12:14:06 -0500

    Improved ellipsoid gizmo

--- Runtime/Utilities/EllipsoidGizmos.cs
+++ Runtime/Utilities/EllipsoidGizmos.cs
@@ -16,13 +16,31 @@ namespace VAT.Shared.Utilities
         {
             var rotation = Quaternion.FromToRotation(Vector3.forward, Vector3.up) * Quaternion.LookRotation(axis);
 
+            var forward = rotation * Vector3.forward;
+            var right = rotation * Vector3.right;
+
             var halfHeight = height * 0.5f;
 
-            EllipseGizmos.DrawEllipse(radius, angle, position - axis * halfHeight, rotation);
+            var top = position + axis * halfHeight;
+            var bottom = position - axis * halfHeight;
 
-            EllipseGizmos.DrawEllipse(radius, angle, position + axis * halfHeight, rotation);
+            EllipseGizmos.DrawEllipse(radius, angle, top, rotation);
 
-            EllipseGizmos.DrawEllipse(radius, angle, position, rotation);
+            EllipseGizmos.DrawEllipse(radius, angle, bottom, rotation);
+
+            float edgeAngle = angle / 4f;
+
+            Vector3 edgeOne = rotation * new Vector3(Mathf.Sin(0f) * radius.x, 0f, Mathf.Cos(0f) * radius.y);
+            Vector3 edgeTwo = rotation * new Vector3(Mathf.Sin(Mathf.Deg2Rad * edgeAngle) * radius.x, 0f, Mathf.Cos(Mathf.Deg2Rad * edgeAngle) * radius.y);
+            Vector3 edgeThree = rotation * new Vector3(Mathf.Sin(Mathf.Deg2Rad * edgeAngle * 2f) * radius.x, 0f, Mathf.Cos(Mathf.Deg2Rad * edgeAngle * 2f) * radius.y);
+            Vector3 edgeFour = rotation * new Vector3(Mathf.Sin(Mathf.Deg2Rad * edgeAngle * 3f) * radius.x, 0f, Mathf.Cos(Mathf.Deg2Rad * edgeAngle * 3f) * radius.y);
+            Vector3 edgeFive = rotation * new Vector3(Mathf.Sin(Mathf.Deg2Rad * edgeAngle * 4f) * radius.x, 0f, Mathf.Cos(Mathf.Deg2Rad * edgeAngle * 4f) * radius.y);
+
+            Gizmos.DrawLine(top + edgeOne, bottom + edgeOne);
+            Gizmos.DrawLine(top + edgeTwo, bottom + edgeTwo);
+            Gizmos.DrawLine(top + edgeThree, bottom + edgeThree);
+            Gizmos.DrawLine(top + edgeFour, bottom + edgeFour);
+            Gizmos.DrawLine(top + edgeFive, bottom + edgeFive);
         }
     }
 }
